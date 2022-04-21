97917dd8acef22f57ac7dc64bd6149c6513ef30d
Nelson <punkpunkada@gmail.com>
2022-04-21 18:49:25 -0300

    criando metodo post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {id: 1, "tilulo": "Tenet"},
     {id: 2, "titulo": "O Resgate do Soldade Ryan"}
@@ -15,6 +17,11 @@ app.get('/livros', (req,res) =>{
     res.status(200).json(livros);
 } );
 
+app.post('/livros', (req,res)=>{
+    livros.push(req.body);
+    res.status(201).send('livro cadastrado com sucesso');
+    //res.status(201).json(livros[2]);
+});
 
 
 export default app;
